e44e03ca7289b1fd663a42f3beef68ed98350583
rgueldenpfennig <mail@robin-gueldenpfennig.de>
2019-01-08 14:42:09 +0100

    add sample project

---
+++ AppMetricsNLogTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using App.Metrics;
+using App.Metrics.AspNetCore;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+
+namespace AppMetricsNLogTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var metrics = AppMetrics.CreateDefaultBuilder()
+                .Build();
+
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureMetrics(metrics)
+                .UseMetrics()
+                .UseMetricsWebTracking()
+                .ConfigureLogging(
+                    (context, builder) =>
+                    {
+                        builder.ClearProviders();
+                        builder.AddConfiguration(context.Configuration.GetSection("Logging"));
+                    })
+                .UseStartup<Startup>()
+                .UseNLog();
+        }
+    }
+}
